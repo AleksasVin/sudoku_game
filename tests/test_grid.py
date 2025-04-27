ccb08b06af57db7da1d568946ee7ca1d0d984dbb
Aleksas V. <vindasiusaleksas@gmail.com>
2025-04-28 02:05:34 +0300

    tests

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -4,20 +4,21 @@ import numpy as np
 from grid import Grid
 from settings import GameSettings
 
+
 class TestGrid(unittest.TestCase):
     def setUp(self):
         pygame.init()
         self.settings = GameSettings()
         self.solution = [
-            [5,3,4,6,7,8,9,1,2],
-            [6,7,2,1,9,5,3,4,8],
-            [1,9,8,3,4,2,5,6,7],
-            [8,5,9,7,6,1,4,2,3],
-            [4,2,6,8,5,3,7,9,1],
-            [7,1,3,9,2,4,8,5,6],
-            [9,6,1,5,3,7,2,8,4],
-            [2,8,7,4,1,9,6,3,5],
-            [3,4,5,2,8,6,1,7,9]
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9]
         ]
         self.puzzle = [row.copy() for row in self.solution]
         # Remove some numbers
@@ -39,7 +40,6 @@ class TestGrid(unittest.TestCase):
         self.grid.select_cell(0, 1)  # Empty cell at (0,1)
         self.assertEqual(self.grid._cells[0][1].value, 0)
 
-
         self.grid.set_value(3)
         self.assertEqual(self.grid._cells[0][1].value, 3)
         self.assertTrue(self.grid._cells[0][1]._is_correct)
@@ -55,7 +55,7 @@ class TestGrid(unittest.TestCase):
         self.grid.set_value(3)  # Correct
         self.grid.select_cell(3, 8)
         self.grid.set_value(3)  # Correct
-        
+
         self.assertTrue(self.grid.victory)
 
     def test_invalid_move(self):
@@ -65,6 +65,7 @@ class TestGrid(unittest.TestCase):
 
     def tearDown(self):
         pygame.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
